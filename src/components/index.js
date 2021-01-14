ea72b391bdd8578e9c50b6518f13e719d7200074
Hamza Shaikh <shaikhhamza157@gmail.com>
2021-01-14 17:13:27 +0530

    changed line 73 from - to +

--- src/components/index.js
+++ src/components/index.js
@@ -70,7 +70,7 @@ const W = 0;
 function normalise(rec) {
   return {
     x1: rec.top ? parseInt(rec.top) : (T - (parseInt(rec.bottom) + parseInt(rec.height))),
-    y1: rec.left? parseInt(rec.left): (W - (parseInt(rec.right) - parseInt(rec.width))),
+    y1: rec.left? parseInt(rec.left): (W - (parseInt(rec.right) + parseInt(rec.width))),
     x2: rec.bottom? (T - parseInt(rec.bottom)): (parseInt(rec.top) + parseInt(rec.height)),
     y2: rec.right? (W - parseInt(rec.right)): (parseInt(rec.left) + parseInt(rec.width)),
   };
